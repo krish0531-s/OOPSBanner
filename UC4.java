4950325bfa696bc09361c392dfac92e600a750b8
krish0531-s <krishpro692@gmail.com>
2026-03-11 09:05:43 +0530

    Added UC4

---
+++ UC4.java
@@ -0,0 +1,18 @@
+public class UC4 {
+    public static void main(String[] args) {
+
+        String[] banner = {
+            " *****   *****   *****   ***** ",
+            "*     * *     * *     * *     *",
+            "*     * *     * *     * *     *",
+            "*     * *     * *****   ***** ",
+            "*     * *     * *       *     ",
+            "*     * *     * *       *     ",
+            " *****   *****  *        ***** "
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
